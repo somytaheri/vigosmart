81e55f22ebab2530fe78e33670a108aa6b587d71
Somi Taheri <somy.taheri@gmail.com>
2014-10-27 20:48:37 +1100

    javascript files added

---
+++ include/js/google-map.js
@@ -0,0 +1,16 @@
+"use strict";
+      function initialize() {
+        var mapOptions = {
+          center: { lat:-37.828, lng: 144.948},
+          zoom: 10,
+          disableDefaultUI:true
+        };
+        var map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+
+
