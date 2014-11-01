95e2c5788d2b0f75e199e530026a54ea505849ab
Somi <somy.taheri@gmail.com>
2014-11-01 12:19:39 +1100

    include added

---
+++ include/js/google-map.js
@@ -0,0 +1,23 @@
+// "use strict";
+// if(window.location.href.indexOf('contact')  != -1){
+// 	function initialize() {
+// 	  var mapOptions = {
+// 	    center: { lat:-37.828, lng: 144.948},
+// 	    zoom: 10,
+// 	    disableDefaultUI:true
+// 	  };
+// 	  var map = new google.maps.Map(document.getElementById('map-canvas'),
+// 	      mapOptions);
+// 	}
+// 	google.maps.event.addDomListener(window, 'load', initialize);
+// };
+
+
+
+
+
+
+
+
+
+
